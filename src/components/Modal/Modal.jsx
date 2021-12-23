ade86d4eb667faa4d5dc9a549e4121e6328cce49
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-12-23 20:35:10 +0100

    added animation on the modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { setShow } from "../../redux/actions/actions";
 import LogIn from "./LogIn";
@@ -8,11 +9,13 @@ function Modal({ logIn }) {
 
   const dispatch = useDispatch() 
 
+  const {homeModal} = useSelector(state => state.home);
+
   return (
     <div className="flex justify-center md:bg-transparent font-sans text-white font-bold items-center overflow-x-hidden overflow-y-auto fixed inset-0 bg-blend-overlay"
     onClick={() => dispatch(setShow(false))}
     >
-      <div className="w-full h-full bg-neutral-900 md:w-96 md:h-96 z-40 drop-shadow-2xl rounded-lg overflow-hidden"
+      <div className={`w-full h-full bg-neutral-900 md:w-96 md:h-96 z-40 drop-shadow-2xl rounded-lg overflow-hidden scale-up-center`}
        onClick={(e) => e.stopPropagation()}
       >
         <div className="flex justify-end">
